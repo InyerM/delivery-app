29768b92a8fe2243236b47edf39b148141eb3f02
Inyer Marín <81336145+InyerM@users.noreply.github.com>
2023-03-24 20:26:42 -0500

    Add common module and update Prettier configuration in Git commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { CommonModule } from "./common/common.module"
+import { Module } from "@nestjs/common"
+import { AppController } from "./app.controller"
 
 @Module({
-  imports: [],
+  imports: [CommonModule],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
