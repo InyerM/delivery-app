17f56a003517e20a78c700373326a768988b9936
Inyer Marín <81336145+InyerM@users.noreply.github.com>
2023-04-05 19:45:37 -0500

    Delete unnecessary files and code related to the delivery-app

--- apps/delivery-app/src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { ConfigurationModule } from "../../../libs/shared/src/configuration/configuration.module"
-import { CommonModule } from "./common/common.module"
-import { Module } from "@nestjs/common"
-
-@Module({
-  imports: [ConfigurationModule, CommonModule],
-})
-export class AppModule {}
