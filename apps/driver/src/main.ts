d6a83eb3c8d5a95d6daa30c131fb238d247f061d
Inyer Marín <81336145+InyerM@users.noreply.github.com>
2023-04-06 12:41:50 -0500

    Add Admin module, controller, service, and e2e tests with "Hello World!" response

--- apps/driver/src/main.ts
+++ apps/driver/src/main.ts
@@ -2,11 +2,12 @@ import { NestFactory } from "@nestjs/core";
 import { DriverModule } from "./driver.module";
 import { ValidationPipe } from "@nestjs/common";
 import { config } from "dotenv";
+import { LoggerService } from "@app/shared/logger/logger.service";
 
 config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(DriverModule, { snapshot: true });
+  const app = await NestFactory.create(DriverModule, { snapshot: true, bufferLogs: true });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -14,6 +15,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  app.useLogger(app.get(LoggerService));
   await app.listen(process.env.DRIVER_PORT);
 }
 bootstrap();
--- apps/shop/src/main.ts
+++ apps/shop/src/main.ts
@@ -2,11 +2,12 @@ import { NestFactory } from "@nestjs/core";
 import { ShopModule } from "./shop.module";
 import { ValidationPipe } from "@nestjs/common";
 import { config } from "dotenv";
+import { LoggerService } from "@app/shared/logger/logger.service";
 
 config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(ShopModule, { snapshot: true });
+  const app = await NestFactory.create(ShopModule, { snapshot: true, bufferLogs: true });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -14,6 +15,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  app.useLogger(app.get(LoggerService));
   await app.listen(process.env.SHOP_PORT);
 }
 bootstrap();
