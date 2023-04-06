c837cc20ead1de2a4a6f802a752cbe031c5071ff
Inyer Marín <81336145+InyerM@users.noreply.github.com>
2023-04-06 12:43:39 -0500

    Delete admin controller, service, and spec files, add logger and validation pipe to app in main.ts

--- apps/admin/src/main.ts
+++ apps/admin/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from "@nestjs/core";
 import { AdminModule } from "./admin.module";
+import { LoggerService } from "@app/shared/logger/logger.service";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AdminModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AdminModule, { snapshot: true, bufferLogs: true });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  app.useLogger(app.get(LoggerService));
+  await app.listen(process.env.ADMIN_PORT);
 }
 bootstrap();
